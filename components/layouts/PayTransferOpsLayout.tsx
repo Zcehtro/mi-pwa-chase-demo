ec3db9dfa87ecdfc13c238b45ba7cbcb53c6b807
Zcehtro <stefano.n.giaimo@gmail.com>
2022-08-19 15:54:20 -0300

    pay_transfer terminado sin funcionalidad

--- components/layouts/PayTransferOpsLayout.tsx
+++ components/layouts/PayTransferOpsLayout.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 
 import {
+  Avatar,
   Box,
   FormControlClasses,
   Typography,
@@ -24,22 +25,13 @@ export const PayTransferGridItem: FC<Props> = ({ icon, label }) => {
       margin="0"
       padding="0"
     >
-      <Grid2
-        display="flex"
-        justifyContent="center"
-        textAlign="center"
-        margin="0"
-        padding="0"
-      >
-        {icon}
+      <Grid2 display="flex" justifyContent="center" textAlign="center" margin="1rem" padding="30px 0 0">
+        <Avatar alt={label} sx={{ backgroundColor: "white", width: 80, height: 80 }}>
+          {icon}
+        </Avatar>
       </Grid2>
       <Grid2 textAlign="center" margin="0" padding="0">
-        <Typography
-          color="primary.main"
-          display="flex"
-          justifyContent="center"
-          fontWeight="bold"
-        >
+        <Typography color="primary.main" display="flex" justifyContent="center" fontWeight="bold">
           {label}
         </Typography>
       </Grid2>
