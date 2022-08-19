81d6b9a896ccf96fbd04f480531f6c88cd8251ee
Zcehtro <stefano.n.giaimo@gmail.com>
2022-08-19 18:33:59 -0300

    vista de payment/transfer con drawer customizable modularizacion de los botones implementación de rutas en BottomNavigation

--- components/layouts/PayTransferOpsLayout.tsx
+++ components/layouts/PayTransferOpsLayout.tsx
@@ -1,40 +1,78 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import {
   Avatar,
   Box,
-  FormControlClasses,
+  Drawer,
   Typography,
   Unstable_Grid2 as Grid2,
 } from "@mui/material";
-import { CreditCardOutlined } from "@mui/icons-material";
 
 interface Props {
   icon: any;
   label: string;
+  drawerContents: any;
 }
 
-export const PayTransferGridItem: FC<Props> = ({ icon, label }) => {
+type Anchor = "bottom";
+
+export const PayTransferGridItem: FC<Props> = ({ icon, label, drawerContents }) => {
+  const [anchorDirection, setAnchorDirection] = useState({ bottom: false });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setAnchorDirection({ ...anchorDirection, [anchor]: open });
+    };
+
   return (
-    <Grid2
-      xs={4}
-      md={2}
-      display="flex"
-      justifyContent="center"
-      flexDirection="column"
-      margin="0"
-      padding="0"
-    >
-      <Grid2 display="flex" justifyContent="center" textAlign="center" margin="1rem" padding="30px 0 0">
-        <Avatar alt={label} sx={{ backgroundColor: "white", width: 80, height: 80 }}>
-          {icon}
-        </Avatar>
+    <>
+      <Grid2
+        xs={4}
+        md={2}
+        display="flex"
+        justifyContent="center"
+        flexDirection="column"
+        margin="0"
+        padding="0"
+      >
+        <Grid2
+          display="flex"
+          justifyContent="center"
+          textAlign="center"
+          margin="1rem"
+          padding="30px 0 0"
+        >
+          <Avatar
+            alt={label}
+            sx={{ backgroundColor: "white", width: 80, height: 80 }}
+            onClick={toggleDrawer("bottom", true)}
+          >
+            {icon}
+          </Avatar>
+        </Grid2>
+        <Grid2 textAlign="center" margin="0" padding="0">
+          <Typography color="primary.main" display="flex" justifyContent="center" fontWeight="bold">
+            {label}
+          </Typography>
+        </Grid2>
       </Grid2>
-      <Grid2 textAlign="center" margin="0" padding="0">
-        <Typography color="primary.main" display="flex" justifyContent="center" fontWeight="bold">
-          {label}
-        </Typography>
-      </Grid2>
-    </Grid2>
+
+      <Drawer
+        anchor={"bottom"}
+        open={anchorDirection["bottom"]}
+        onClose={toggleDrawer("bottom", false)}
+      >
+        <Typography variant="h5" align="center" sx={{margin: "1rem 0 0.6rem" }}>{label}</Typography>
+        {drawerContents}
+      </Drawer>
+    </>
   );
 };
--- components/ui/_shared/BottomNavigator.tsx
+++ components/ui/_shared/BottomNavigator.tsx
@@ -1,26 +1,25 @@
 import * as React from "react";
-import { Box, Paper } from "@mui/material";
+import { Paper } from "@mui/material";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faWallet,
-  faSackDollar,
-  faFilePen,
-  faStar,
-  faChartLine,
-} from "@fortawesome/free-solid-svg-icons";
-import {
-  AccountBalanceOutlined,
   AccountBalanceWalletOutlined,
   CurrencyExchange,
   HistoryEduOutlined,
   ShowChartOutlined,
   StarBorderOutlined,
 } from "@mui/icons-material";
+import { useRouter } from "next/router";
+
 
 export function BottomNavigator() {
   const [value, setValue] = React.useState(0);
+  
+  const router = useRouter();
+  
+  const goLink = (href: string) => {
+    router.push(href);
+  };
 
   return (
     <Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }} elevation={3}>
@@ -36,11 +35,13 @@ export function BottomNavigator() {
           label="Accounts"
           icon={<AccountBalanceWalletOutlined />}
           sx={{ padding: "0 2px" }}
-        />
+          onClick={() => goLink("/")}
+          />
         <BottomNavigationAction
           label="Pay & Transfer"
           icon={<CurrencyExchange />}
           sx={{ padding: "0 2px" }}
+          onClick={() => goLink("/pay_transfer")}
         />
         <BottomNavigationAction
           label="Plan & Track"
