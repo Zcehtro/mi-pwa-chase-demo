4f801d467248dbeba04fa6cc0460386def6a4965
Zcehtro <stefano.n.giaimo@gmail.com>
2022-08-24 09:32:24 -0300

    comandos para lighthouse actualizados

---
+++ lighthouse/runLighthouse-deployed.bat
@@ -0,0 +1,6 @@
+@REM Para correr lighthouse desde la máquina, instalar a través de NPM como paquete global con la siguiente instrucción:
+@REM npm install lighthouse --global
+@REM 
+@REM Y luego ejecutar la última línea o simplemente ejecutar este archivo.
+@REM Este comando ejecutará lighthouse sobre la versión deployada en la web.
+lighthouse https://magenta-innova-pwa-chase-demo.vercel.app --view --save-assets
