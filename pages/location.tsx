64432f04ff4b5f62a73b005438ebc75adee6bfcb
elFonTii <feluu.contacto@gmail.com>
2022-08-31 11:11:17 -0300

    useGeolocation and ./location page

---
+++ pages/location.tsx
@@ -0,0 +1,32 @@
+import { MainLayout } from "../components/layouts/MainLayout";
+import { NextPage } from "next";
+import { Box, Typography } from "@mui/material";
+import { useGeolocated } from "react-geolocated";
+
+const Location: NextPage = () => {
+  const { coords } = useGeolocated({
+    positionOptions: { enableHighAccuracy: true },
+  });
+
+  return (
+    <MainLayout>
+      <Box display="flex" flexDirection="column" gap={2} p={3}>
+        <Typography variant="h5" fontWeight="bold" sx={{ color: "#555" }}>
+          Location
+        </Typography>
+        <Typography variant="body1" sx={{ color: "#555" }}>
+          In the future, this will be a map that displays the closest bank to
+          you.
+        </Typography>
+        <Typography variant="h6" sx={{ color: "#555" }}>
+          Coordinates
+        </Typography>
+        <Typography variant="body1" sx={{ color: "#555" }}>
+          {`${coords?.latitude}, ${coords?.longitude}`}
+        </Typography>
+      </Box>
+    </MainLayout>
+  );
+};
+
+export default Location;
